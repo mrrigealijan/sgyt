97e69fa9fe464f673c9fbaf6dc8c59b4e0406a9f
rigeirani <ccaahh430@gmail.com>
2016-02-16 14:25:12 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {185658347,107746299,75213323,139990182,121069994},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
